e6fddd1d94ace3e6efd01ae3b76ba3874fd31fcc
HancaUwU <hana.fielko@gmail.com>
2025-06-10 10:27:06 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . e e e f . . . . . 
+    . . e e d d d d e f . . . 
+    . e 1 d d d d d d 1 e . . 
+    e 1 1 1 d d d 1 1 1 1 e . 
+    d d d 1 1 f f f f d 1 d . 
+    d d d d f e e e e f d 1 . 
+    e d d f f e e f f f d e . 
+    f f f b c e e c b f f f . 
+    . f 4 1 a 4 4 a 1 4 f . . 
+    . f 4 4 4 4 3 4 4 4 f . . 
+    . . . f e e e e f . . . . 
+    . f f 6 9 9 9 9 6 f f . . 
+    e 4 f 9 9 9 9 9 9 f 4 e . 
+    . e f 6 6 6 6 6 6 f e . . 
+    . . . f f f f f f . . . . 
+    . . . c c . . c c . . . . 
+    `, SpriteKind.Player)
